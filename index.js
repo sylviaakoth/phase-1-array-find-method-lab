b4b1104863d5a9c396363d52ee2985d7bda1556a
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-23 15:47:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(records) {
+    const foundRecord = records.find((record) => {
+        return record.result === 'W'
+    }) 
+    
+    if(foundRecord) {
+        return foundRecord.year 
+    } else {
+        return undefined
+    }
+}
+
+console.log(superbowlWin(record));
